8b628aee729031fb601e568cd2ff2179f26df69f
Dragonaere <astill@dragonaere.com>
2024-02-18 16:31:04 -0500

    Switched to MUI and added Color Modes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,58 @@
-import type { Metadata } from 'next'
+import { Metadata, Viewport } from 'next'
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
+import getInitColorSchemeScript from "@mui/system/cssVars/getInitColorSchemeScript";
+import { Experimental_CssVarsProvider } from "@mui/material";
+
 import './globals.css'
 
+import Theme from "../providers/ColorMode";
+
 export const metadata: Metadata = {
-  title: 'Dragonaere Enterprises',
-  description: 'Coming Soon',
+  metadataBase: new URL('https://www.dragonaere.com'),
+  title: {
+    default: 'Dragonaere Enterprises',
+    template: '%s | Dragonaere Enterprises',
+  },
+  description: 'Dragonaere Enterprises | Coming Soon',
+  creator: 'Andrew Still',
+  referrer: 'origin-when-cross-origin',
+  formatDetection: {
+    email: false,
+    telephone: false,
+    address: false,
+    date: false,
+    url: false,
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false
+  },
+  twitter: {
+    card: 'summary',
+    title: 'Dragonaere Enterprises',
+    description: 'Dragonaere Enterprises | Coming Soon',
+    creator: '@DragonaereEnt'
+  },
+  openGraph: {
+    title: 'Dragonaere Enterprises',
+    description: 'Dragonaere Enterprises | Coming Soon',
+    url: 'https://www.dragonaere.com',
+    siteName: 'Dragonaere Enterprises',
+    type: 'website',
+    locale: 'en_US'
+  },
+  icons: {
+    icon: 'https://images.static.dragonaere.com/favicon.ico',
+    shortcut: 'https://images.static.dragonaere.com/favicon.ico'
+  }
+}
+
+export const viewport:Viewport = {
+  colorScheme: 'dark light',
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
 }
 
 export default function RootLayout({
@@ -13,7 +62,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AppRouterCacheProvider>
+          <Experimental_CssVarsProvider defaultMode="system">
+            {getInitColorSchemeScript({
+              attribute: "data-mui-color-scheme",
+              modeStorageKey: "mui-mode",
+              colorSchemeStorageKey: "mui-color-scheme",
+              defaultMode: "system",
+            })}
+            <Theme>
+              {children}
+            </Theme>
+          </Experimental_CssVarsProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,35 @@
+import { Box } from "@mui/system";
+
 export default function Home() {
   return (
-    <main className="main">
-      <div className="head">
-        <h1 className="title orange">Dragonaere</h1>
-        <h1 className="title">&nbsp;</h1>
-        <h1 className="title">Enterprises</h1>
-      </div>
-      <div className='mainbody'>
-        <p className='subtitle'>
-          Coming Soon™
-        </p>
-      </div>
-      <div className="footer">
-        <p>© 2024 Dragonaere Enterprises</p>
-      </div>
+    <main>
+      <Box className='main'>
+        <Box className='head'>
+          <h1 className='title'>Dragonaere</h1>
+          <h1 className="title">&nbsp;</h1>
+          <h1 className='title orange'>Enterprises</h1>
+        </Box>
+        <Box className='mainbody' display="flex" flexDirection="column">
+          <p className='subtitle'>
+            Coming Soon™
+          </p>
+          {/* <Box display="grid" gridTemplateColumns="repeat(2, 1fr)" gridTemplateRows="1fr" justifyContent="center" alignItems="center">
+            <p className='links animate__animated animate__zoomIn animate__delay-2s'>
+              <Link href="/links">
+                My Socials
+              </Link>
+            </p>
+            <p className='links animate__animated animate__zoomIn animate__delay-2s'>
+              <Link href="/about">
+                About Me
+              </Link>
+            </p>
+          </Box> */}
+        </Box>
+        <Box className="footer">
+          <p>© 2024 Dragonaere Enterprises</p>
+        </Box>
+      </Box>
     </main>
   )
 }
